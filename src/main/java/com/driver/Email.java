3a9a804b640aac98974ea5c99f586297ba9f3a51
VamshiRk27 <vamshi.rk27@gmail.com>
2023-04-02 01:36:27 +0530

    Attempt 1

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -5,6 +5,9 @@ public class Email {
     private String emailId;
     private String password;
 
+    public Email() {
+    }
+
     public Email(String emailId){
         this.emailId = emailId;
         this.password = "Accio@123";
@@ -25,5 +28,47 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(getPassword().equals(oldPassword)){
+            if(newPassword.length()>=8 && checkUpperCase(newPassword) && checkLowerCase(newPassword) && checkDigit(newPassword) && checkSpecial(newPassword)){
+                password=newPassword;
+            }
+        }
+    }
+    public boolean checkUpperCase(String password){
+        for(int i=0;i<password.length();i++){
+            if('A'-password.charAt(i)>=0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkLowerCase(String password){
+        for(int i=0;i<password.length();i++){
+            if(password.charAt(i)-'a'>=0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkDigit(String password){
+        for(int i=0;i<password.length();i++){
+            if(password.charAt(i)-'0'>=0 && password.charAt(i)-'0'<=9){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkSpecial(String password){
+        for(int i=0;i<password.length();i++){
+            if (!Character.isDigit(password.charAt(i))
+                    && !Character.isLetter(password.charAt(i))
+                    && !Character.isWhitespace(password.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -12,12 +12,15 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId,Integer.MAX_VALUE);
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-
+        if(calendar==null){
+            calendar = new ArrayList<>();
+        }
+        calendar.add(meeting);
     }
 
     public int findMaxMeetings(){
@@ -25,6 +28,14 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-
+        int count=1;
+        for(int i=0;i<calendar.size()-1;i++){
+            Meeting m1=calendar.get(i);
+            Meeting m2=calendar.get(i+1);
+            if(m1.getEndTime().isBefore(m2.getStartTime())){
+                count++;
+            }
+        }
+        return count;
     }
 }
